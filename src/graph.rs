db44b0f560ad0e69b45a90100a8b2483f80e906d
lazy-sock <paul.k@unity-mail.de>
2026-02-01 17:34:55 +0100

    add basic display trait

--- src/graph.rs
+++ src/graph.rs
@@ -1,15 +1,30 @@
+use std::fmt;
 use std::rc::Rc;
 
-struct Graph {
+pub struct Graph {
     value: i32,
     nodes: Vec<Rc<Graph>>,
 }
 
-fn get_random_graph() -> Graph {
+impl fmt::Display for Graph {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.value)
+    }
+}
+
+pub fn get_random_graph() -> Graph {
     let mut root = Graph {
         value: 42,
         nodes: vec![],
     };
+
+    for i in 0..5 {
+        let node = Graph {
+            value: i,
+            nodes: vec![],
+        };
+        root.nodes.push(Rc::from(node));
+    }
 
     root
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
+use crate::graph::get_random_graph;
+
 fn main() {
     println!("Hello, world!");
+    let graph = get_random_graph();
+    println!("{}", graph);
 }
 
 mod graph;
