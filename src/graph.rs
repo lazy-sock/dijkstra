f4e0a063de6d1da524689d8d792bcb1a8e0bf487
lazy-sock <paul.k@unity-mail.de>
2026-02-02 22:04:22 +0100

    first try at implementing directed Graph

--- src/graph.rs
+++ src/graph.rs
@@ -1,17 +1,24 @@
 use rand::prelude::*;
+use std::cell::RefCell;
 use std::fmt;
 use std::rc::Rc;
 
 pub struct Graph {
     value: i32,
-    nodes: Vec<Rc<Graph>>,
+    nodes: RefCell<Vec<Rc<Edge>>>,
+}
+
+pub struct Edge {
+    start: Rc<Graph>,
+    end: Rc<Graph>,
+    value: i32,
 }
 
 impl Graph {
     fn get_nodes(&self) -> Vec<&Graph> {
         let mut nodes = vec![self];
-        for i in &self.nodes {
-            for j in i.get_nodes() {
+        for i in self.nodes.borrow().iter() {
+            for j in i.end.get_nodes() {
                 nodes.push(j);
             }
         }
@@ -30,26 +37,32 @@ impl fmt::Display for Graph {
     }
 }
 
-pub fn get_random_graph(root_value: Option<i32>) -> Graph {
+pub fn get_random_graph(root_value: Option<i32>) -> Rc<Graph> {
     // root_value is an optional parameter
-    let mut root = Graph {
+    let root = Graph {
         value: root_value.unwrap_or(42),
-        nodes: vec![],
+        nodes: RefCell::from(vec![]),
     };
 
     let number_of_nodes = rand::random_range(0..10);
 
+    let root_rc = Rc::new(root);
+
     for _ in 0..number_of_nodes {
-        let mut node = Graph {
+        let mut node = Rc::from(Graph {
             value: rand::random_range(0..1000),
-            nodes: vec![],
-        };
+            nodes: RefCell::from(vec![]),
+        });
         let has_children = rand::random_bool(0.2);
         if has_children {
             node = get_random_graph(Some(node.value)); // can sometimes produce stack overflow
         }
-        root.nodes.push(Rc::from(node));
+        root_rc.nodes.borrow_mut().push(Rc::from(Edge {
+            start: Rc::clone(&root_rc),
+            end: Rc::from(node),
+            value: 0,
+        }));
     }
 
-    root
+    root_rc
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use crate::graph::get_random_graph;
 
 fn main() {
-    println!("Hello, world!");
     let graph = get_random_graph(None);
     println!("{}", graph);
 }
