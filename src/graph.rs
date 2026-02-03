8749563350e2b5211a6082fe5cf381a6ee6387f1
lazy-sock <paul.k@unity-mail.de>
2026-02-03 19:24:26 +0100

    use adjacency matrix as implementation for graph

--- src/graph.rs
+++ src/graph.rs
@@ -1,68 +1,33 @@
-use rand::prelude::*;
-use std::cell::RefCell;
 use std::fmt;
-use std::rc::Rc;
 
 pub struct Graph {
-    value: i32,
-    nodes: RefCell<Vec<Rc<Edge>>>,
-}
-
-pub struct Edge {
-    start: Rc<Graph>,
-    end: Rc<Graph>,
-    value: i32,
-}
-
-impl Graph {
-    fn get_nodes(&self) -> Vec<&Graph> {
-        let mut nodes = vec![self];
-        for i in self.nodes.borrow().iter() {
-            for j in i.end.get_nodes() {
-                nodes.push(j);
-            }
-        }
-        nodes
-    }
+    matrix: Vec<Vec<i32>>,
 }
 
 impl fmt::Display for Graph {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let nodes = self.get_nodes();
-        let mut values = vec![];
-        for node in nodes {
-            values.push(node.value);
+        let mut matrix = String::from("");
+        for i in &self.matrix {
+            let mut row = String::from("");
+            for j in i {
+                row += &j.to_string();
+            }
+            row += "\n";
+            matrix += &row;
         }
-        write!(f, "{:?}", values)
+        write!(f, "{}", matrix)
     }
 }
 
-pub fn get_random_graph(root_value: Option<i32>) -> Rc<Graph> {
-    // root_value is an optional parameter
-    let root = Graph {
-        value: root_value.unwrap_or(42),
-        nodes: RefCell::from(vec![]),
-    };
-
-    let number_of_nodes = rand::random_range(0..10);
-
-    let root_rc = Rc::new(root);
-
-    for _ in 0..number_of_nodes {
-        let mut node = Rc::from(Graph {
-            value: rand::random_range(0..1000),
-            nodes: RefCell::from(vec![]),
-        });
-        let has_children = rand::random_bool(0.2);
-        if has_children {
-            node = get_random_graph(Some(node.value)); // can sometimes produce stack overflow
+pub fn get_random_graph() -> Graph {
+    let size = rand::random_range(0..10);
+    let mut matrix = vec![];
+    for _ in 0..size {
+        let mut row = vec![];
+        for _ in 0..size {
+            row.push(rand::random_range(0..10));
         }
-        root_rc.nodes.borrow_mut().push(Rc::from(Edge {
-            start: Rc::clone(&root_rc),
-            end: Rc::from(node),
-            value: 0,
-        }));
+        matrix.push(row);
     }
-
-    root_rc
+    Graph { matrix }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use crate::graph::get_random_graph;
 
 fn main() {
-    let graph = get_random_graph(None);
+    let graph = get_random_graph();
     println!("{}", graph);
 }
 
