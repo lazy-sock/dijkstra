8137139403e8a7c8b903f5e3bd3961c7c1ea7176
lazy-sock <paul.k@unity-mail.de>
2026-02-07 19:37:49 +0100

    add more bellman_ford tests

--- src/lib.rs
+++ src/lib.rs
@@ -86,12 +86,6 @@ mod tests {
     use super::*;
 
     #[test]
-    fn bellman_three_nodes_linear() {
-        let matrix = vec![vec![0, 1, i32::MAX], vec![1, 0, 3], vec![i32::MAX, 3, 0]];
-        assert_eq!(bellman_ford(&Graph { matrix }, 0), [0, 1, 4]);
-    }
-
-    #[test]
     fn test_dijkstra_single_node() {
         let matrix = vec![vec![0]];
         assert_eq!(dijkstra(&Graph { matrix }, 0), [0]);
@@ -174,5 +168,90 @@ mod tests {
             vec![6, 3, 2, 0],
         ];
         assert_eq!(dijkstra(&Graph { matrix }, 0), [0, 2, 3, 5]);
+    }
+
+    #[test]
+    fn test_bellman_ford_single_node() {
+        let matrix = vec![vec![0]];
+        assert_eq!(bellman_ford(&Graph { matrix }, 0), [0]);
+    }
+
+    #[test]
+    fn test_bellman_ford_two_nodes() {
+        let matrix = vec![vec![0, 2], vec![2, 0]];
+        assert_eq!(bellman_ford(&Graph { matrix }, 0), [0, 2]);
+    }
+
+    #[test]
+    fn test_bellman_ford_three_nodes_linear() {
+        let matrix = vec![vec![0, 1, i32::MAX], vec![1, 0, 3], vec![i32::MAX, 3, 0]];
+        assert_eq!(bellman_ford(&Graph { matrix }, 0), [0, 1, 4]);
+    }
+
+    #[test]
+    fn test_bellman_ford_three_nodes_triangle() {
+        let matrix = vec![vec![0, 2, 5], vec![2, 0, 1], vec![5, 1, 0]];
+        assert_eq!(bellman_ford(&Graph { matrix }, 0), [0, 2, 3]);
+    }
+
+    #[test]
+    fn test_bellman_ford_four_nodes() {
+        let matrix = vec![
+            vec![0, 4, 2, i32::MAX],
+            vec![4, 0, 1, 5],
+            vec![2, 1, 0, 8],
+            vec![i32::MAX, 5, 8, 0],
+        ];
+        assert_eq!(bellman_ford(&Graph { matrix }, 0), [0, 3, 2, 8]);
+    }
+
+    #[test]
+    fn test_bellman_ford_from_different_source() {
+        let matrix = vec![vec![0, 1, 4], vec![1, 0, 2], vec![4, 2, 0]];
+        assert_eq!(bellman_ford(&Graph { matrix }, 1), [1, 0, 2]);
+    }
+
+    #[test]
+    fn test_bellman_ford_disconnected_nodes() {
+        let matrix = vec![
+            vec![0, 5, i32::MAX],
+            vec![5, 0, i32::MAX],
+            vec![i32::MAX, i32::MAX, 0],
+        ];
+        assert_eq!(bellman_ford(&Graph { matrix }, 0), [0, 5, i32::MAX]);
+    }
+
+    #[test]
+    fn test_bellman_ford_five_nodes_complex() {
+        let matrix = vec![
+            vec![0, 10, i32::MAX, 5, i32::MAX],
+            vec![10, 0, 3, i32::MAX, 7],
+            vec![i32::MAX, 3, 0, i32::MAX, 1],
+            vec![5, i32::MAX, i32::MAX, 0, 2],
+            vec![i32::MAX, 7, 1, 2, 0],
+        ];
+        assert_eq!(bellman_ford(&Graph { matrix }, 0), [0, 10, 8, 5, 7]);
+    }
+
+    #[test]
+    fn test_bellman_ford_all_same_weight() {
+        let matrix = vec![
+            vec![0, 1, 1, 1],
+            vec![1, 0, 1, 1],
+            vec![1, 1, 0, 1],
+            vec![1, 1, 1, 0],
+        ];
+        assert_eq!(bellman_ford(&Graph { matrix }, 0), [0, 1, 1, 1]);
+    }
+
+    #[test]
+    fn test_bellman_ford_fully_connected() {
+        let matrix = vec![
+            vec![0, 2, 4, 6],
+            vec![2, 0, 1, 3],
+            vec![4, 1, 0, 2],
+            vec![6, 3, 2, 0],
+        ];
+        assert_eq!(bellman_ford(&Graph { matrix }, 0), [0, 2, 3, 5]);
     }
 }
